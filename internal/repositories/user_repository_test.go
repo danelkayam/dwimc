86a6727a3caf423c67dd507422baf6c767fc5609
Dan Elkayam <elkayamdan@gmail.com>
2025-02-23 21:00:16 +0200

    WIP - user repository - tests in progress

--- internal/repositories/user_repository_test.go
+++ internal/repositories/user_repository_test.go
@@ -1,35 +1,138 @@
 package repositories
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
+
+	"dwimc/internal/model"
 )
 
+type testUser struct {
+	id       uint
+	email    string
+	password string
+	token    string
+}
+
 func TestUserRepository(t *testing.T) {
-	db := setupTestDB(t)
-	repo := NewSQLUserRepository(db)
+	testUsers := generateTestUser()
 
-	t.Run("create user", func(t *testing.T) {
-		email := "testuser@dwimc.awesome"
-		password := "hashedpass"
-		token := "some-token"
-		createdUser, err := repo.Create(email, password, token)
+	t.Run("create users", func(t *testing.T) {
+		db := setupTestDB(t)
+		repo := NewSQLUserRepository(db)
 
+		for _, testUser := range testUsers {
+			user, err := repo.Create(testUser.email, testUser.password, testUser.token)
+
+			if err != nil {
+				t.Fatalf("Create User failed: %v", err)
+			}
+
+			if user.ID == 0 {
+				t.Fatalf("Expected a valid ID, got 0")
+			}
+
+			if user.Email != testUser.email {
+				t.Fatalf("Got: %s, expected %s (user ID: %d)",
+					user.Email, testUser.email, testUser.id)
+			}
+
+			if user.Password != testUser.password {
+				t.Fatalf("Got: %s, expected %s (user ID: %d)",
+					user.Password, testUser.password, testUser.id)
+			}
+
+			if user.Token.String != testUser.token {
+				t.Fatalf("Got: %s, expected %s (user ID: %d)",
+					user.Token.String, testUser.token, testUser.id)
+			}
+		}
+	})
+
+	t.Run("get user", func(t *testing.T) {
+		t.Parallel()
+		db := setupTestDB(t)
+		repo := NewSQLUserRepository(db)
+
+		user, err := repo.Create(testUsers[0].email, testUsers[0].password, testUsers[0].token)
 		if err != nil {
 			t.Fatalf("Create User failed: %v", err)
 		}
 
-		if createdUser.ID == 0 {
-			t.Fatalf("Expected a valid ID, got 0")
-		}
+		retrieved, err := repo.GetBy(user.ID)
+		checkGetUser(t, retrieved, user, err)
 
-		if email != createdUser.Email {
-			t.Fatalf("Expected email: %s, got %s", email, createdUser.Email)
-		}
-
-		if password != createdUser.Password {
-			t.Fatalf("Expected email: %s, got %s", password, createdUser.Password)
-		}
-
-		t.Logf("Success creating user: %v", createdUser)
+		retrieved, err = repo.GetByEmail(user.Email)
+		checkGetUser(t, retrieved, user, err)
 	})
+
+	t.Run("update user", func(t *testing.T) {
+		t.Parallel()
+		db := setupTestDB(t)
+		repo := NewSQLUserRepository(db)
+
+		_, err := repo.Create(testUsers[0].email, testUsers[0].password, testUsers[0].token)
+		if err != nil {
+			t.Fatalf("Create User failed: %v", err)
+		}
+
+		// TODO - implement this - update password
+		// TODO - implement this - update token
+		// TODO - impelemnt this - update both
+	})
+
+	t.Run("delete user", func(t *testing.T) {
+		t.Parallel()
+		db := setupTestDB(t)
+		repo := NewSQLUserRepository(db)
+
+		user, err := repo.Create(testUsers[0].email, testUsers[0].password, testUsers[0].token)
+		if err != nil {
+			t.Fatalf("Create User failed: %v", err)
+		}
+
+		retrieved, err := repo.GetBy(user.ID)
+		checkGetUser(t, retrieved, user, err)
+
+		err = repo.Delete(user.ID)
+		if err != nil {
+			t.Fatalf("Delete User failed: %v", err)
+		}
+
+		retrieved, err = repo.GetBy(user.ID)
+		if err != nil {
+			t.Fatalf("Get User failed: %v", err)
+		}
+
+		if retrieved != nil {
+			t.Fatalf("Got %+v, expected nil", retrieved)
+		}
+	})
+}
+
+func generateTestUser() []testUser {
+	const size = 10
+	testUsers := make([]testUser, size)
+
+	for i := 0; i < len(testUsers); i++ {
+		testUsers[i] = testUser{
+			id:       uint(i),
+			email:    fmt.Sprintf("user-%d@dwimc.awesome", i),
+			password: fmt.Sprintf("secret-password-%d", i),
+			token:    fmt.Sprintf("token-%d", i),
+		}
+	}
+
+	return testUsers
+}
+
+func checkGetUser(t *testing.T, retrievedUser *model.User, user *model.User, err error) {
+	if err != nil {
+		t.Fatalf("Failed to get user: %v", err)
+	}
+
+	if !reflect.DeepEqual(retrievedUser, user) {
+		t.Fatalf("Got %+v, expected %+v", retrievedUser, user)
+	}
 }
