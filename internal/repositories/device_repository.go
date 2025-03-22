5686617102d2982b5f28b9fea1e45d3ad5230e70
Dan Elkayam <elkayamdan@gmail.com>
2025-03-22 18:18:35 +0200

    wip: repositories implementation

--- internal/repositories/device_repository.go
+++ internal/repositories/device_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"context"
 	"dwimc/internal/model"
+	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -19,7 +20,7 @@ type DeviceRepository interface {
 }
 
 type MongodbDeviceRepository struct {
-	context context.Context
+	context    context.Context
 	collection *mongo.Collection
 }
 
@@ -42,27 +43,102 @@ func NewMongodbDeviceRepository(
 	}
 
 	return &MongodbDeviceRepository{
-		context: context,
+		context:    context,
 		collection: collection,
 	}, nil
 }
 
 func (r *MongodbDeviceRepository) GetDevices() ([]model.Device, error) {
-	// TODO - implement this
-	return nil, nil
+	cursor, err := r.collection.Find(r.context, bson.M{})
+	if err != nil {
+		// TODO - handle db errors
+		return nil, err
+	}
+
+	defer cursor.Close(r.context)
+	devices := []model.Device{}
+
+	for cursor.Next(r.context) {
+		var device model.Device
+
+		if err := cursor.Decode(&device); err != nil {
+			// TODO - handle db errors
+			return nil, err
+		}
+
+		devices = append(devices, device)
+	}
+
+	return devices, nil
 }
 
 func (r *MongodbDeviceRepository) GetDevice(serial string) (*model.Device, error) {
-	// TODO - implement this
-	return nil, nil
+	var device model.Device
+
+	err := r.collection.FindOne(
+		r.context,
+		bson.M{"serial": serial},
+	).Decode(&device)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			// TODO - return ItemNotFoundError?
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return &device, nil
 }
 
 func (r *MongodbDeviceRepository) CreateDevice(serial, name string) (*model.Device, error) {
-	// TODO - implement this
-	return nil, nil
+	var device model.Device
+
+	updatedAt := time.Now().UTC()
+	filter := bson.M{"serial": serial}
+	update := bson.M{
+		"$set": bson.M{
+			"serial":    serial,
+			"name":      name,
+			"updatedAt": updatedAt,
+		},
+		"$setOnInsert": bson.M{"createdAt": updatedAt},
+	}
+
+	opts := options.FindOneAndUpdate().
+		SetUpsert(true).
+		SetReturnDocument(options.After)
+
+	err := r.collection.FindOneAndUpdate(
+		r.context,
+		filter,
+		update,
+		opts,
+	).Decode(&device)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			// TODO - return ItemNotFoundError
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return &device, nil
 }
 
 func (r *MongodbDeviceRepository) DeleteDevice(serial string) error {
-	// TODO - implement this
+	_, err := r.collection.DeleteOne(
+		r.context,
+		bson.M{"serial": serial},
+	)
+
+	if err != nil {
+		// TODO - handle db errors
+		return err
+	}
+
 	return nil
 }
--- internal/repositories/location_repository.go
+++ internal/repositories/location_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"context"
 	"dwimc/internal/model"
+	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -10,6 +11,7 @@ import (
 )
 
 const COLLECTION_NAME_LOCATIONS = "locations"
+
 type LocationRepository interface {
 	GetLocations(serial string) ([]model.Location, error)
 	GetLatestLocation(serial string) (*model.Location, error)
@@ -41,27 +43,90 @@ func NewMongodbLocationRepository(
 	}
 
 	return &MongodbLocationRepository{
-		context: context,
+		context:    context,
 		collection: collection,
 	}, nil
 }
 
 func (r *MongodbLocationRepository) GetLocations(serial string) ([]model.Location, error) {
-	// TODO - implement this
-	return nil, nil
+	cursor, err := r.collection.Find(
+		r.context,
+		bson.M{"deviceSerial": serial},
+	)
+
+	if err != nil {
+		// TODO - handle db errors
+		return nil, err
+	}
+
+	defer cursor.Close(r.context)
+	locations := []model.Location{}
+
+	for cursor.Next(r.context) {
+		var location model.Location
+
+		if err := cursor.Decode(&location); err != nil {
+			// TODO - handle db errors
+			return nil, err
+		}
+
+		locations = append(locations, location)
+	}
+
+	return locations, nil
 }
 
 func (r *MongodbLocationRepository) GetLatestLocation(serial string) (*model.Location, error) {
-	// TODO - implement this
-	return nil, nil
+	var location model.Location
+
+	err := r.collection.FindOne(
+		r.context,
+		bson.M{"deviceSerial": serial},
+		options.FindOne().SetSort(bson.D{{Key: "updatedAt", Value: -1}}),
+	).Decode(&location)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			// TODO - return ItemNotFoundError?
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &location, nil
 }
 
 func (r *MongodbLocationRepository) CreateLocation(location model.Location) (*model.Location, error) {
-	// TODO - implement this
-	return nil, nil
+	created := time.Now().UTC()
+
+	location.ID = bson.NewObjectID().String()
+	location.CreatedAt = created
+	location.UpdatedAt = created
+
+	result, err := r.collection.InsertOne(r.context, location)
+	if err != nil {
+		// TODO - handle db errors
+		return nil, err
+	}
+
+	if result.InsertedID == nil {
+		// TODO - handle db errors
+		return nil, nil
+	}
+
+	return &location, nil
 }
 
 func (r *MongodbLocationRepository) DeleteLocation(id string) error {
-	// TODO - implement this
+	_, err := r.collection.DeleteOne(
+		r.context,
+		bson.M{"_id": id},
+	)
+
+	if err != nil {
+		// TODO - handle db errors
+		return err
+	}
+
 	return nil
 }
